cc3f8d4d9f471985394ac98d7212fe6f03657b6e
Ankush Jethi <ankush.jethi@in.ibm.com>
2020-09-30 17:37:54 +0100

    My First Checkin

---
+++ server/src/main/java/com/example/Git.java
@@ -0,0 +1,24 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello How are you, %s!", someone);
+    system.out.println("Hello Git Ankush"):
+        system.out.println("Develop Branch Change 1"):
+            system.out.println("1_GitStuff Change_1 coming"):
+
+
+  }
+}
---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,29 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello How are you, %s!", someone);
+    system.out.println("Hello Git Ankush"):
+       system.out.println("DevelopBranch_Change1"):
+    	   system.out.println("1_GitStuff Change_1 coming"):
+  }
+  
+  public String India(String State) {
+	    return String.format("Hello How are you, %s!", someone);
+	    system.out.println("Hello Git Ankush"):
+	       system.out.println("DevelopBranch_Change1"):
+	    	   system.out.println("1_GitStuff Change_1 coming"):
+	  }
+}
